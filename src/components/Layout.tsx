76e85eb6aee89b8bb5bfe8ace8f779ccbb2c5761
Abdul-Majeed-Ansari <abdulmajeed31136@gmail.com>
2025-02-21 00:01:12 -0800

    "COnditional Rendering in Layout.tsx"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,25 @@
-import Navbar from './Navbar'
-import Footer from './Footer'
-import Topbar from './Topbar'
+import { ReactNode } from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = ({children}) => {
-  return (
-    <div>
-      <Topbar />
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+  const hideLayoutFor = ["/login", "/register"];
+
+  return hideLayoutFor.includes(location.pathname) ? (
+    <>{children}</>
+  ) : (
+    <>
       <Navbar />
       {children}
       <Footer />
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Layout
+export default Layout;

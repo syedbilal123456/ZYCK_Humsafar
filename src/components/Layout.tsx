c68064bbd75b6c85fc6530dc6535238c8aee6e49
Abdul-Majeed-Ansari <abdulmajeed31136@gmail.com>
2025-02-21 02:40:18 -0800

    "."

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Topbar from "./Topbar";
 
 interface LayoutProps {
   children: ReactNode;
@@ -15,6 +16,7 @@ const Layout = ({ children }: LayoutProps) => {
     <>{children}</>
   ) : (
     <>
+      <Topbar />
       <Navbar />
       {children}
       <Footer />
